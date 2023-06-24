0027bd6ff12ae05562de220ae41e2529f1a2e82a
azarias.boutin <azarias.boutin@luxquanta.com>
2023-06-24 19:48:53 +0200

    Show select language when screen is a certain size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,16 +16,18 @@
         <q-select
           v-model="locale"
           :options="localeOptions"
-          :hide-selected="true"
-          :hide-dropdown-icon="true"
+          :hide-selected="$q.screen.lt.md"
+          :hide-dropdown-icon="$q.screen.lt.md"
+          label-color="white"
+          :dark="true"
+          :options-dark="false"
           dense
           :borderless="true"
           emit-value
           map-options
-          dropdown-icon="translate"
         >
           <template v-slot:prepend>
-            <q-icon color="white" name="translate"/>
+            <q-icon color="white" name="translate" />
           </template>
         </q-select>
         <q-btn @click="$q.dark.toggle" flat dense round icon="compare"></q-btn>
@@ -35,7 +37,7 @@
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header>{{ $t('navigation.navigation') }}</q-item-label>
-        <EssentialLink v-for="link in links" :key="link.title" v-bind="link"/>
+        <EssentialLink v-for="link in links" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -44,7 +46,7 @@
         <p class="text-center text-caption q-pl-sm q-mb-none">
           Copyright Mishi © 2023 - All right reserved
         </p>
-        <q-space/>
+        <q-space />
         <q-btn
           flat
           round
@@ -63,17 +65,17 @@
     </q-footer>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
-import {Measurements} from 'src/logic/constants';
-import {storeToRefs} from 'pinia';
-import {useI18n} from 'vue-i18n';
-import {useMeasurementStore} from 'stores/measurement-store';
+import { defineComponent, ref } from 'vue';
+import { Measurements } from 'src/logic/constants';
+import { storeToRefs } from 'pinia';
+import { useI18n } from 'vue-i18n';
+import { useMeasurementStore } from 'stores/measurement-store';
 import EssentialLink from 'components/EssentialLink.vue';
 
 const links = [
@@ -103,22 +105,22 @@ const links = [
 
 export default defineComponent({
   name: 'MainLayout',
-  components: {EssentialLink},
+  components: { EssentialLink },
 
   setup() {
     const leftDrawerOpen = ref(false);
     const store = useMeasurementStore();
-    const {measurement} = storeToRefs(store);
-    const {locale, t} = useI18n({useScope: 'global'});
+    const { measurement } = storeToRefs(store);
+    const { locale, t } = useI18n({ useScope: 'global' });
 
     return {
       Measurements,
       measurement,
       locale,
       localeOptions: [
-        {value: 'en-US', label: 'English'},
-        {value: 'fr', label: 'Français'},
-        {value: 'es-ES', label: 'Español'},
+        { value: 'en-US', label: 'English' },
+        { value: 'fr', label: 'Français' },
+        { value: 'es-ES', label: 'Español' },
       ],
       measurementOptions: Measurements.map((name) => ({
         label: t(`measurements.${name}`),
